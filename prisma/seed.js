92968024ad40c947354cc3702935ab75b660c7b0
AlvifSandana <asm240800@gmail.com>
2023-02-25 21:41:57 +0700

    [alvif] : add seeder (ongoing)

---
+++ prisma/seed.js
@@ -0,0 +1,34 @@
+const PrismaClient = require('@prisma/client').PrismaClient
+const prisma = new PrismaClient()
+const passwordGen = require('../src/v1/utils/password').passwordGen
+
+async function main() {
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        username: 'alice',
+        password: passwordGen('alice'),
+      },
+      {
+        username: 'bob',
+        password: passwordGen('bob'),
+      },
+      {
+        username: 'carol',
+        password: passwordGen('carol'),
+      }
+    ],
+    skipDuplicates: true,
+  })
+  console.log({ users })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
