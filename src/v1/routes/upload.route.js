e0bbfe32bc62fde52f11e80b98d7a91e9417e465
kimearwud22 <kimearwud22@gmail.com>
2023-07-06 06:30:59 +0700

    update route

--- src/v1/routes/upload.route.js
+++ src/v1/routes/upload.route.js
@@ -6,7 +6,7 @@ const validateToken = require('../middlewares/validateToken.middleware');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../../../uploads'));
+    cb(null, path.join(__dirname, '/../../../uploads'));
   },
   filename: (req, file, cb) => {
     // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.' +file.originalname.split('.')[1];
